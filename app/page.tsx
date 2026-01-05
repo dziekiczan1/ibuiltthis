6853ec9f71918ae93c4b5feb522898dc2ebc2f09
dzekiczan1 <p.rzadkowolski@gmail.com>
2026-01-05 14:19:08 +0100

    feat(products): add product skeleton components and dynamic product page; update landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
+import { Suspense } from "react";
+
 import HeroSection from "@/components/landing-page/hero-section";
 import RecentlyLaunchedProducts from "@/components/landing-page/recently-launched-products";
 import FeaturedProducts from "@/components/landing-page/featured-products";
+import ProductSkeleton from "@/components/products/product-skeleton";
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
       <FeaturedProducts />
-      <RecentlyLaunchedProducts />
+      <Suspense fallback={<ProductSkeleton />}>
+        <RecentlyLaunchedProducts />
+      </Suspense>
     </div>
   );
 }
---
+++ app/products/[slug]/page.tsx
@@ -0,0 +1,20 @@
+"use cache";
+
+import { getFeaturedProducts } from "@/lib/products/product-select";
+
+export const generateStaticParams = async () => {
+  const products = await getFeaturedProducts();
+  return products.map((product) => ({
+    slug: product.slug.toString(),
+  }));
+};
+
+export default async function Product({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
+
+  return <div>product {slug}</div>;
+}
